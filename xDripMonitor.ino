4ec9a0ffc954a6f5a60aeed9ee479b3af692e005
dgavagnin <dgavagnin@hce.it>
2024-01-11 12:18:35 +0100

    rimossa la data e aggiunto il tempo trascorso (NTP), aggiustamenti al valore delta

--- xDripMonitor.ino
+++ xDripMonitor.ino
@@ -7,12 +7,18 @@
 #include <Wire.h>
 
 #include <stdint.h>
+#include <time.h>
 
 #include "settings.h"
 
 // WIFI settings
 const char* ssid = WIFI_SSID;
 const char* password = WIFI_PASS;
+
+// NTP server to request epoch time
+const char* ntpServer = "pool.ntp.org";
+// Variable to save current epoch time
+unsigned long epochTime = 0; 
 
 //Your Domain name with URL path or IP address with path
 String serverName = "http://motog625g:17580";
@@ -29,10 +35,19 @@ unsigned long timerDelay = 30000;
 U8G2_SSD1306_128X64_NONAME_F_SW_I2C u8g2(U8G2_R0,/* clock or SCL=*/4,/* data or SDA=*/5,/* reset=*/ U8X8_PIN_NONE);
 
 unsigned long currentMillis = 0;   // stores the value of millis() in each iteration of loop()
+unsigned long delayedMillis = 0;   // stores the value of millis() in each iteration of loop() delayed from wificonnection
 const int wifiBlinkInterval = 500; // number of millisecs between blinks 
 const int wifiRetryInterval = 500; // number of millisecs between retry to connect to wifi 
 unsigned long previousWifiBlinkMillis = 0;
 bool wifiBlinkStatus = false;
+
+// POSITIONS {x,y}
+const int arrowUpPosition[] = {95,0};
+const int arrowFlatPosition[] = {95,27};
+const int arrowDownPosition[] = {95,42};
+
+const int deltaPosition[] = {0,64};
+const int datePosition[] = {50,64};
 
 void setup() {
   Serial.begin(115200);
@@ -47,7 +62,8 @@ void setup() {
   u8g2.sendBuffer();
 
   WiFi.begin(ssid, password);
-
+  configTime(0, 0, ntpServer);
+  
   // OLED test
   printMissingPhone();
   printWifi();
@@ -55,10 +71,12 @@ void setup() {
   printMainText("888");
   printArrow("Flat");
   printArrow("FortyFiveDown");
+  printArrow("FortyFiveUp");
+  printArrow("DoubleUp");
   printArrow("SingleDown");
   printArrow("DoubleDown");
-  printDelta("+0.007");
-  printDate("00-00-0000 00:00");
+  printDelta(8);
+  printDate(8);
   delay(2000);
   removeDate();
   removeDelta();
@@ -79,6 +97,7 @@ void loop() {
     return;
   }
   if (!wifiBlinkStatus) printWifi();
+  
 
   // main loop
   /*Serial.println("Main loop timing:");
@@ -102,10 +121,16 @@ void loop() {
     Serial.println(httpResponseCode);
 
     if (httpResponseCode == 200) {
+      delayedMillis = millis();
+      if (epochTime == 0) { 
+        epochTime = getTime();
+        Serial.print("Epoch Time: ");
+        Serial.println(epochTime);
+      }
       removeMissingPhone();
       String response = http.getString();
       JSONVar myObject = JSON.parse(response);
-
+      Serial.println(response);
       // JSON.typeof(jsonVar) can be used to get the type of the var
       if (JSON.typeof(myObject) == "undefined") {
         printMainText("ERR");
@@ -114,6 +139,7 @@ void loop() {
       } else {
         removeJsonError();
         // Get infos from response
+        /*
         String ts = myObject[0]["dateString"];
         int TIndex = ts.indexOf('T');
   //       Serial.println("dateString: " + ts);
@@ -135,26 +161,44 @@ void loop() {
         String day = dateEn;
   //       Serial.println("day: " + day);
 
-
         String timeMs = ts.substring(TIndex + 1, ts.length());
         int MsIndex = timeMs.indexOf('.');
         String time = timeMs.substring(0, MsIndex);
+*/
+        int minutesElapsed = 0;
+        if (epochTime > 0) {
+          String dateStr = JSON.stringify(myObject[0]["date"]);
+          dateStr = dateStr.substring(0, 10);
+          Serial.println("dateStr: " + dateStr);
+          Serial.print("epochTime: ");
+          Serial.println(epochTime);
+          Serial.print("delayedMillis: ");
+          Serial.println(delayedMillis);
+          int now = (delayedMillis/1000) + epochTime;
+          Serial.print("now: ");
+          Serial.println(now);
+          minutesElapsed = (now - (dateStr.toInt()))/60;
+          Serial.print("elapsed: ");
+          Serial.println(minutesElapsed);
+        }
 
         String sgv = JSON.stringify(myObject[0]["sgv"]);
-        String delta = JSON.stringify(myObject[0]["delta"]); // + " mg/dl";
+        int delta = int(myObject[0]["delta"]); // + " mg/dl";
         String direction = JSON.stringify(myObject[0]["direction"]);
         direction.replace("\"", "");
-        String date = day + '/' + month + '/' + year + ' ' + time;
+        // String date = day + '/' + month + '/' + year + ' ' + time;
 
         Serial.println(sgv);
         Serial.println(delta);
         Serial.println(direction);
-        Serial.println(date);
+        Serial.println(minutesElapsed);
 
         printMainText(sgv);
         printArrow(direction);
         printDelta(delta);
-        printDate(date);
+        if (epochTime > 0) {
+          printDate(minutesElapsed);
+        }
       }
     } else {
       removeMainText();
@@ -168,34 +212,51 @@ void loop() {
   }
 }
 
-void printDate(String text) {
+// Function that gets current epoch time
+unsigned long getTime() {
+  time_t now;
+  struct tm timeinfo;
+  if (!getLocalTime(&timeinfo)) {
+    Serial.println("Failed to obtain time");
+    return(0);
+  }
+  time(&now);
+  return now;
+}
+
+void printDate(int value) {
+  String text = String(value);
+  text = text + " min fa";
   removeDate();
   u8g2.setDrawColor(1);
   u8g2.setFont(u8g2_font_courR08_tf); //7x11
-  u8g2.drawStr(0,64,text.c_str());
+  u8g2.drawStr(datePosition[0],datePosition[1],text.c_str());
   u8g2.sendBuffer(); 
 }
 void removeDate() {
   u8g2.setDrawColor(0);
-  u8g2.drawBox(0,57,120,11);
+  u8g2.drawBox(datePosition[0],datePosition[1]-7,120,11);
   u8g2.sendBuffer();
 }
 
-void printDelta(String text) {
+void printDelta(int value) {
+  String text = String(value);
   if (!text.startsWith("-") && !text.startsWith("+")) {
     text = "+" + text;
   }
   removeDelta();
   u8g2.setDrawColor(1);
-  u8g2.setFont(u8g2_font_courR08_tf); //7x11
-  u8g2.drawStr(0,55,text.c_str()); 
-  //u8g2.setFont(u8g2_font_04b_03b_tr); //5x6
-  //u8g2.drawStr(text.length()*5,50,"mg/dl"); 
+  u8g2.setFont(u8g2_font_crox5h_tn); //12x25
+  u8g2.drawStr(deltaPosition[0],deltaPosition[1],text.c_str()); 
+  u8g2.setFont(u8g2_font_04b_03b_tr); //5x6
+  u8g2.drawStr(deltaPosition[0] + (text.length()*15),deltaPosition[1]-12,"mg/dl"); 
   u8g2.sendBuffer(); 
 }
 void removeDelta() {
   u8g2.setDrawColor(0);
-  u8g2.drawBox(0,48,50,11);
+  u8g2.drawBox(deltaPosition[0],deltaPosition[1]-20,26,20);
+  u8g2.setDrawColor(0);
+  u8g2.drawBox(deltaPosition[0],deltaPosition[1]-20,54,10);
   u8g2.sendBuffer();
 }
 
@@ -206,45 +267,45 @@ void printArrow(String direction) {
   if (direction == "Flat") {
     Serial.print("Print arrow: ");
     Serial.println(direction);
-    u8g2.drawUTF8(52,58,"\u2b0c");
+    u8g2.drawUTF8(arrowFlatPosition[0],arrowFlatPosition[1],"\u2b0c");
   }
   if (direction == "FortyFiveDown") {
     Serial.print("Print arrow: ");
     Serial.println(direction);
-    u8g2.drawUTF8(52,58,"\u2b0a");
+    u8g2.drawUTF8(arrowDownPosition[0],arrowDownPosition[1],"\u2b0a");
   }
   if (direction == "FortyFiveUp") {
     Serial.print("Print arrow: ");
     Serial.println(direction);
-    u8g2.drawUTF8(52,58,"\u2b08");
+    u8g2.drawUTF8(arrowUpPosition[0],arrowUpPosition[1],"\u2b08");
   }
   if (direction == "SingleDown") {
     Serial.print("Print arrow: ");
     Serial.println(direction);
-    u8g2.drawUTF8(52,58,"\u2b07");
+    u8g2.drawUTF8(arrowDownPosition[0],arrowDownPosition[1],"\u2b07");
   }
   if (direction == "SingleUp") {
     Serial.print("Print arrow: ");
     Serial.println(direction);
-    u8g2.drawUTF8(52,58,"\u2b06");
+    u8g2.drawUTF8(arrowUpPosition[0],arrowUpPosition[1],"\u2b06");
   }
   if (direction == "DoubleDown") {
     Serial.print("Print arrow: ");
     Serial.println(direction);
-    u8g2.drawUTF8(52,58,"\u2b07");
-    u8g2.drawUTF8(62,58,"\u2b07");
+    u8g2.drawUTF8(arrowDownPosition[0],arrowDownPosition[1],"\u2b07");
+    u8g2.drawUTF8(arrowDownPosition[0]+7,arrowDownPosition[1],"\u2b07");
   }
   if (direction == "DoubleUp") {
     Serial.print("Print arrow: ");
     Serial.println(direction);
-    u8g2.drawUTF8(52,58,"\u2b06");
-    u8g2.drawUTF8(62,58,"\u2b06");
+    u8g2.drawUTF8(arrowUpPosition[0],arrowUpPosition[1],"\u2b06");
+    u8g2.drawUTF8(arrowUpPosition[0]+7,arrowUpPosition[1],"\u2b06");
   }
   u8g2.sendBuffer();
 }
 void removeArrow() {
   u8g2.setDrawColor(0);
-  u8g2.drawBox(52,42,20,20);
+  u8g2.drawBox(arrowUpPosition[0],arrowUpPosition[1],20,42);
   u8g2.sendBuffer();
 }
 
